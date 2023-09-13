2a692d19eef3415847fde5e0fc7a0f00b0d8f872
Rafayel <mikaelyan.rafael20@gmail.com>
2023-09-14 00:50:13 +0400

    Test Rafayel

---
+++ testChess/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  testChess
+//
+//  Created by Saqo on 14.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello")
+        // Do any additional setup after loading the view.
+        print("Test By Rafayel")
+    }
+
+
+}
+
