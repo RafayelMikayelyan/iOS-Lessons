03b7e8368a273357086f685eec77bb89123ced43
Rafayel <mikaelyan.rafael20@gmail.com>
2023-09-14 11:43:57 +0400

    Test Rafayel

--- testChess/ViewController.swift
+++ testChess/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello")
         // Do any additional setup after loading the view.
+        print("Test By Rafayel")
     }
 
 
